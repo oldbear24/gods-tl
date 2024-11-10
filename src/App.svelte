1ac90579afec0ebc01a7a1cdfcfc0e0e14155a1c
oldbear24 <david@nadvornik.cz>
2024-11-10 17:00:53 +0100

    setup basic structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,43 @@
-<button class="btn">Hello daisyui</button>
+<div class="navbar bg-base-100">
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl">GoDS TL Attendace</a>
+  </div>
+  <div class="flex-none">
+    <button class="btn btn-square btn-ghost">
+    Log In
+    </button>
+  </div>
+</div>
+
+<div class="overflow-x-auto">
+  <table class="table">
+    <!-- head -->
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- row 1 -->
+      <tr>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+      </tr>
+      <!-- row 2 -->
+      <tr>
+        <td>Hart Hagerty</td>
+        <td>Desktop Support Technician</td>
+        <td>Purple</td>
+      </tr>
+      <!-- row 3 -->
+      <tr>
+        <td>Brice Swyre</td>
+        <td>Tax Accountant</td>
+        <td>Red</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
